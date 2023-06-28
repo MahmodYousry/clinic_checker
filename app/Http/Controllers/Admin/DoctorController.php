62d19ecf099c88afba831f02dd048178ff23d8db
mahmod <yousry.mohmmed@rocketmail.com>
2023-06-29 00:36:47 +0300

    Module Doctors -- Admin Dashboard -- Finished

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -43,7 +43,7 @@ class DoctorController extends Controller
             $file = $request->file('photo');
 
             // Make Folder with doctors_photos if not exist then move the photo there
-            $file->storeAs('doctors_photos/', $photoName);
+            $file->storeAs('public/doctors_photos', $photoName);
 
             // Store Every Field
             $newStore = new Doctor();
@@ -60,26 +60,139 @@ class DoctorController extends Controller
 
         }
 
-        return redirect()->route('doctors.index')->with('success', 'Data Has Been Saved Successfuly !');
+        return redirect()->route('doctors.index')->with('success', 'Doctor Data Has Been Saved Successfuly !');
     }
 
     public function show(string $id)
     {
-        //
+        $doctor = Doctor::findOrFail($id);
+        return view('dashboard.admin.show', compact('doctor'));
     }
 
     public function edit($id)
     {
-        return view('dashboard.admin.edit');
+        $doctor = Doctor::findOrFail($id);
+        return view('dashboard.admin.edit', compact('doctor'));
     }
 
     public function update(Request $request, string $id)
     {
-        //
+
+        // Validate First
+        $valiation = $request->validate([
+            'first_name'        => 'required',
+            'last_name'         => 'required',
+            'email'             => 'required|email',
+            'gender'            => 'required',
+            'photo'             => 'image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
+            'phone'             => 'required',
+            'speciality'        => 'required',
+            'address'           => 'required',
+            'facebook_profile'  => 'required',
+        ]);
+
+        // get the doctor data using the id
+        $editDoctor = Doctor::findOrFail($id);
+
+        if ( $request->hasfile('photo') ) { // if file selected then
+
+            // Asign Photo Name
+            $photoName = $request->photo->getClientOriginalName();
+            $file = $request->file('photo');
+
+            // upload and update the new one
+
+            // get the old photo path
+            $path = '../storage/app/doctors_photos/'.$editDoctor->photo;
+
+            if (file_exists($path)) { // file exist
+
+                // delete the file
+                unlink($path);
+
+                // Make Folder with doctors_photos if not exist then move the photo there
+                $file->storeAs('public/doctors_photos', $photoName);
+
+                // Update Every Field
+                $editDoctor->first_name           = $request->first_name;
+                $editDoctor->last_name            = $request->last_name;
+                $editDoctor->email                = $request->email;
+                $editDoctor->gender               = $request->gender;
+                $editDoctor->photo                = $photoName;
+                $editDoctor->phone                = $request->phone;
+                $editDoctor->speciality           = $request->speciality;
+                $editDoctor->address              = $request->address;
+                $editDoctor->facebook_page_link   = $request->facebook_profile;
+                $editDoctor->save();
+
+            } else { // file does not exist so continue
+
+                // Make Folder with doctors_photos if not exist then move the photo there
+                $file->storeAs('public/doctors_photos', $photoName);
+
+                // Update Every Field
+                $editDoctor->first_name           = $request->first_name;
+                $editDoctor->last_name            = $request->last_name;
+                $editDoctor->email                = $request->email;
+                $editDoctor->gender               = $request->gender;
+                $editDoctor->photo                = $photoName;
+                $editDoctor->phone                = $request->phone;
+                $editDoctor->speciality           = $request->speciality;
+                $editDoctor->address              = $request->address;
+                $editDoctor->facebook_page_link   = $request->facebook_profile;
+                $editDoctor->save();
+
+            }
+
+            // return redirect
+            return redirect()->route('doctors.index')->with('success', 'Doctor Data Updated successfuly');
+
+        } else {
+
+            // Update Every Field Except Photo
+            $editDoctor->first_name           = $request->first_name;
+            $editDoctor->last_name            = $request->last_name;
+            $editDoctor->email                = $request->email;
+            $editDoctor->gender               = $request->gender;
+            $editDoctor->phone                = $request->phone;
+            $editDoctor->speciality           = $request->speciality;
+            $editDoctor->address              = $request->address;
+            $editDoctor->facebook_page_link   = $request->facebook_profile;
+            $editDoctor->save();
+
+            return redirect()->route('doctors.index')->with('success', 'Doctor Data Updated successfuly');
+
+        }
     }
 
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        // get the row from db using the id of the doctor
+        $row = Doctor::findOrFail($id);
+
+        // if there is image in db
+        if ($row->photo) {
+            // get the full path
+            $path = '../storage/app/doctors_photos/'.$row->photo;
+            // delete file using path if file exist and delete the row
+            if (file_exists($path)) {
+                // delete the file
+                unlink($path);
+                // delete the row in db
+                $row->delete();
+                // return success message
+                return redirect()->route('doctors.index')->with('success', 'Doctor Data had deleted');
+            } else {
+                // delete the row in db
+                $row->delete();
+                // return success message
+                return redirect()->route('doctors.index')->with('success', 'Doctor Data had deleted but his photo file not found on the server');
+            }
+        } else {
+            // delete the row in db
+            $row->delete();
+            // return success message
+            return redirect()->route('doctors.index')->with('success', 'Doctor Data had deleted but no photo found in database to delete');
+        }
     }
 }
--- resources/views/dashboard/admin/show.blade.php
+++ resources/views/dashboard/admin/show.blade.php
@@ -62,28 +62,40 @@
             @endif
             {{-- end errors And Alerts --}}
             <a class="btn btn-primary mb-4" href="{{ route('doctors.create') }}"><i class="fa fa-plus-square mr-2"></i> Register New Doctor</a>
-            <div class="row">
-                {{-- start doctors cards --}}
-                @foreach ($doctors as $doctor)
-                    <div class="col-md-6 col-lg-4 col-xl-3">
-                        <div class="card mb-4 border-0" data-toggle="appear" data-offset="50" data-class="animated fadeIn">
-                            <img class="card-img-top" style="max-height: 345px;" src="{{ $doctor->photo ? asset('storage/doctors_photos/'.$doctor->photo) : asset('img/defualt.avif') }}" alt="Card image cap">
-                            <div class="card-body">
-                                <h5 class="h5 mb-1">{{ $doctor->first_name }}</h5>
-                                <p class="font-size-sm text-muted">{{ $doctor->speciality }}</p>
-                                <a href="{{ route('doctors.show', $doctor->id) }}" class="btn btn-info"><i class="fa fa-eye p-1"></i></a>
-                                <a href="{{ route('doctors.edit', $doctor->id) }}" class="btn btn-primary"><i class="fa fa-edit p-1"></i></a>
+            <a class="btn btn-primary mb-4" href="{{ url()->previous() }}"><i class="fa fa-arrow-left mr-2"></i> Back</a>
+            <div class="block block-rounded">
+                <div class="block block-header">
+                    <h3 class="block-title">Doctor Full Info</h3>
+                </div>
+                <div class="block block-content">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-4 col-xl-3">
+
+                            <h5 class="h5 mb-4">Name: {{ $doctor->first_name }}</h5>
+
+                            <img class="border border-dark rounded img-fluid" style="max-height: 345px;" src="{{ $doctor->photo ? asset('storage/doctors_photos/'.$doctor->photo) : asset('img/defualt.avif') }}" alt="Card image cap">
+
+                            <p class="font-size-sm text-muted">speciality : {{ $doctor->speciality }}</p>
+                            <p class="font-size-sm text-muted">email : {{ $doctor->email }}</p>
+                            <p class="font-size-sm text-muted">gender :{{ $doctor->gender }}</p>
+                            <p class="font-size-sm text-muted">last_name :{{ $doctor->last_name }}</p>
+                            <p class="font-size-sm text-muted">address : {{ $doctor->address }}</p>
+                            <p class="font-size-sm text-muted">facebook_page_link :{{ $doctor->facebook_page_link }}</p>
+
+                            <div class="control mb-3">
+                                <a href="{{ route('doctors.edit', $doctor->id) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit p-1"></i></a>
                                 <form action="{{ route('doctors.destroy', $doctor->id) }}" method="POST" class="d-inline-block">
                                     @csrf
                                     @method('DELETE')
-                                    <button class="btn btn-danger" type="submit"><i class="fa fa-trash p-1"></i></button>
+                                    <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash p-1"></i></button>
                                 </form>
                             </div>
+
                         </div>
                     </div>
-                @endforeach
-                {{-- End doctors cards --}}
+                </div>
             </div>
+
         </div>
 
     </main>
