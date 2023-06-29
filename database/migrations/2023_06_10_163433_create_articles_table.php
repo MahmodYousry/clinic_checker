f3b3533a4aebed49c4bc62d066bd40ceb14397a7
JohnAndria <johnandria90@gmail.com>
2023-06-29 16:44:25 +0300

    fix error article

--- database/migrations/2023_06_10_163433_create_articles_table.php
+++ database/migrations/2023_06_10_163433_create_articles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('cover_image');
-            $table->text('article');
+            $table->longText('article');
             $table->timestamps();
         });
     }
