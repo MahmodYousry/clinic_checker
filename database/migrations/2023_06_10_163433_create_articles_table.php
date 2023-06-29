9f2b5c8b8ec2a0d9477aa54d3e04b02697767987
mahmod <yousry.mohmmed@rocketmail.com>
2023-06-29 17:11:06 +0300

    Articles -- Patient Dashboard -- Finished2 conflict done

--- database/migrations/2023_06_10_163433_create_articles_table.php
+++ database/migrations/2023_06_10_163433_create_articles_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
 
             $table->string('title');
             $table->string('cover_image');
-            $table->text('article');
+            $table->longText('article');
 
             $table->timestamps();
         });
